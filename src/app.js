f5099ba8073ce09719cd78f91d7739a611cf3fd7
juliangoaguirre <143230248+juliangoaguirre@users.noreply.github.com>
2023-09-04 15:29:43 +0000

    completed excuse generator

--- src/app.js
+++ src/app.js
@@ -8,4 +8,29 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whorandom = Math.floor(Math.random() * who.length);
+  let actionrandom = Math.floor(Math.random() * action.length);
+  let whatrandom = Math.floor(Math.random() * what.length);
+  let whenrandom = Math.floor(Math.random() * when.length);
+
+  excuse.innerHTML =
+    who[whorandom] +
+    " " +
+    action[actionrandom] +
+    " " +
+    what[whatrandom] +
+    " " +
+    when[whenrandom];
 };
